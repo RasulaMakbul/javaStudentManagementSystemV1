3315d4be4e4a0a9a3266113f21f89b1694ca7fac
RasulaMakbul <emu.cfs@gmail.com>
2020-10-11 17:03:07 +0600

    Java Student management SYstem

---
+++ StudentDatabaseManagementSystem/src/studentdatabasemanagementsystem/FXMLDocumentController.java
@@ -0,0 +1,26 @@
+
+package studentdatabasemanagementsystem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class FXMLDocumentController implements Initializable {
+    
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+
+    @FXML
+    private void addAllStudentBtnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void ViewAllStudentBtnAction(ActionEvent event) {
+    }
+    
+}
